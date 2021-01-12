b3c9b28cc5ed424e3183d28788a40d24e9370af8
JaesungMoon <jsdekiru@gmail.com>
2021-01-13 02:29:59 +0900

    Removed LunchScreen

---
+++ BottomTabLayoutTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BottomTabLayoutTest
+//
+//  Created by JaesungMoon on 2021/01/13.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
