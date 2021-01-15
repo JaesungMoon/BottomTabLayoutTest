741810be40d24ab9663fd82224d005baf69f2779
JaesungMoon <jsdekiru@gmail.com>
2021-01-16 02:35:33 +0900

    Fixed order

--- BottomTabLayoutTest/Tab3ViewController.swift
+++ BottomTabLayoutTest/Tab3ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class Tab3ViewController: UIViewController {
     
+    @IBOutlet weak var navItem: UINavigationItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Tab3ViewController:viewDidLoad")
         view.backgroundColor = .yellow
+        
+        navItem.title = "TTT"
     }
     
 }
